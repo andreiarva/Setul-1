5689cd7a7f730d26ca6d613da67bcc2088bd97d0
andreiarva <115580916+andreiarva@users.noreply.github.com>
2022-11-30 22:55:01 +0200

    Add files via upload

---
+++ Problema1/Problema1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rezolvati ecuatia de gradul 1 cu o necunoscuta: ax+b = 0, unde a si b sunt date de intrare. ");
+            string[] t = Console.ReadLine().Split(' ');
+            int b = int.Parse(t[0]);
+            int a = int.Parse(t[1]);
+            int x = -(b / a);
+            Console.WriteLine("x =" + " " + x);
+            Console.ReadKey();
+
+        }
+    }
+}
